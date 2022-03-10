e1a177c8df958c9e1c4b9c498654a0d1aca6e211
greedthestampede <roburolireis@gmail.com>
2022-03-10 00:20:57 -0300

    Adicionando as descrições e as pontuações da Ficha de Personagem

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
+import Head from "next/head";
+import { Conteudo, Entrada, MeuLink, Texto, Titulo } from "../src/components/tema";
+
 export default function Home() {
-    return <h1>Vrau</h1>
+  return (
+    <Conteudo>
+      <Head>
+        <title>Character Sheet - School RPG</title>
+      </Head>
+      <Titulo>Character Sheet</Titulo>
+      <Entrada
+        type="text"
+        autoCapitalize="words"
+        name="Name"
+        placeholder="Name"
+      />
+      <Entrada
+        type="text"
+        autoCapitalize="words"
+        name="Class"
+        placeholder="Class"
+      />
+      <Entrada
+        type="text"
+        autoCapitalize="words"
+        name="Ranking"
+        placeholder="Ranking"
+      />
+      <Texto><MeuLink href="/">+</MeuLink> 0 <MeuLink href="/">-</MeuLink> (XP)</Texto>
+      <Texto><MeuLink href="/">+</MeuLink> 0 <MeuLink href="/">-</MeuLink> (FP)</Texto>
+    </Conteudo>
+  );
 }
---
+++ src/components/tema.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Titulo = styled.h1`
+  color: azure;
+`;
+
+export const Conteudo = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: justify;
+  font-family: 'Source Sans Pro', sans-serif;
+  padding: 20%;
+  padding-top: 5%;
+  height: 585px;
+  margin: 0;
+  background-color: black;
+`;
+
+export const Entrada = styled.input`
+  color: azure;
+  background-color: black;
+  margin-top: 5px;
+`;
+
+export const Texto = styled.p`
+  color: azure;
+  margin: 5px;
+`;
+
+export const MeuLink = styled.a`
+  color: azure;
+  text-decoration: none;
+`;
