47359e64c28dcd81145b6593f5cb75538a83daf6
greedthestampede <roburolireis@gmail.com>
2022-03-11 13:03:50 -0300

    Finalizando Styled-Components e criando um footer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import Head from "next/head";
-import styled from "styled-components";
-import { setCookie, parseCookies } from 'nookies';
+import styled, { createGlobalStyle } from "styled-components";
+import { setCookie, parseCookies } from "nookies";
 
-const Titulo = styled.h1`
-  color: azure;
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: black;
+    font-family: "Source Sans Pro", sans-serif;
+    margin:0;
+    padding:0;
+    height:100%;
+  }
 `;
 
 const Conteudo = styled.section`
@@ -12,18 +18,24 @@ const Conteudo = styled.section`
   flex-direction: column;
   align-items: center;
   text-align: justify;
-  font-family: 'Source Sans Pro', sans-serif;
-  padding: 20%;
-  padding-top: 5%;
-  height: 585px;
-  margin: 0;
-  background-color: black;
+  padding: 10px;
+  padding-bottom: 60px;
+  min-height: 100%;
+  position: relative;
+`;
+
+const Titulo = styled.h1`
+  color: azure;
 `;
 
 const Entrada = styled.input`
   color: azure;
   background-color: black;
+  border: 1px solid rgba(15, 133, 97, 45%);
+  border-width: 2px;
+  border-radius: 5px;
   margin-top: 5px;
+  text-align: center;
 `;
 
 const Texto = styled.p`
@@ -44,101 +56,134 @@ const Btn = styled.button`
   border-radius: 100%;
 `;
 
+const Pezinho = styled.footer`
+  color: azure;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  padding-top: 25px;
+  text-align: center;
+  background-color: rgba(15, 133, 97, 50%);
+  border: 1px solid;
+  border-color: azure;
+  border-radius: 10px;
+`;
+
 export default function Home(props) {
   const [name, setName] = useState(props.NOME);
   const [classe, setClasse] = useState(props.CLASSE);
   const [ranking, setRanking] = useState(props.RANKING);
   const [xp, setXP] = useState(props.XP);
   const [fp, setFP] = useState(props.FP);
-  
-  const sobeXP = () => {if (typeof xp === 'number') {
-    setXP(xp + 1)
-  } else {setXP(0)};
-    setCookie(null, 'XP', xp + 1, {
+
+  const sobeXP = () => {
+    if (typeof xp === "number") {
+      setXP(xp + 1);
+    } else {
+      setXP(0);
+    }
+    setCookie(null, "XP", xp + 1, {
       maxAge: 86400 * 120,
-      path: '/',
-    });};
-  const desceXP = () => {if (typeof xp === 'number' && xp >= 1) {
-    setXP(xp - 1)
+      path: "/",
+    });
   };
-    setCookie(null, 'XP', xp - 1, {
+  const desceXP = () => {
+    if (typeof xp === "number" && xp >= 1) {
+      setXP(xp - 1);
+    }
+    setCookie(null, "XP", xp - 1, {
       maxAge: 86400 * 120,
-      path: '/',
-    });};
-  const sobeFP = () => {if (typeof xp === 'number') {
-    setFP(fp + 1)
-  } else {setFP(0)};
-    setCookie(null, 'FP', fp + 1, {
-      maxAge: 86400 * 120,
-      path: '/',
-    });};
-  const desceFP = () => {if (typeof fp === 'number' && fp >= 1) {
-    setFP(fp - 1)
+      path: "/",
+    });
   };
-    setCookie(null, 'FP', fp - 1, {
+  const sobeFP = () => {
+    if (typeof xp === "number") {
+      setFP(fp + 1);
+    } else {
+      setFP(0);
+    }
+    setCookie(null, "FP", fp + 1, {
       maxAge: 86400 * 120,
-      path: '/',
-    });};
+      path: "/",
+    });
+  };
+  const desceFP = () => {
+    if (typeof fp === "number" && fp >= 1) {
+      setFP(fp - 1);
+    }
+    setCookie(null, "FP", fp - 1, {
+      maxAge: 86400 * 120,
+      path: "/",
+    });
+  };
 
   function handleNameChange(evento) {
     setName(evento.target.value);
-    setCookie(null, 'NOME', name, {
+    setCookie(null, "NOME", name, {
       maxAge: 86400 * 120,
-      path: '/',
+      path: "/",
     });
   }
   function handleClassChange(evento) {
     setClasse(evento.target.value);
-    setCookie(null, 'CLASSE', classe, {
+    setCookie(null, "CLASSE", classe, {
       maxAge: 86400 * 120,
-      path: '/',
+      path: "/",
     });
   }
   function handleRankingChange(evento) {
     setRanking(evento.target.value);
-    setCookie(null, 'RANKING', ranking, {
+    setCookie(null, "RANKING", ranking, {
       maxAge: 86400 * 120,
-      path: '/',
+      path: "/",
     });
   }
-  
+
   return (
-    <Conteudo>
+    <>
       <Head>
         <title>Character Sheet - School RPG</title>
       </Head>
-      <Titulo>Character Sheet</Titulo>
-      <Entrada
-        type="text"
-        autoCapitalize="words"
-        name="Name"
-        value={name}
-        onChange={handleNameChange}
-        placeholder="Name"
-      />
-      <Entrada
-        type="text"
-        autoCapitalize="words"
-        name="Class"
-        value={classe}
-        onChange={handleClassChange}
-        placeholder="Class"
-      />
-      <Entrada
-        type="text"
-        autoCapitalize="words"
-        name="Ranking"
-        value={ranking}
-        onChange={handleRankingChange}
-        placeholder="Ranking"
-      />
-      <Texto>
-        <Btn onClick={sobeXP}>+</Btn> {xp} <Btn onClick={desceXP}>-</Btn> (XP)
-      </Texto>
-      <Texto>
-        <Btn onClick={sobeFP}>+</Btn> {fp} <Btn onClick={desceFP}>-</Btn> (FP)
-      </Texto>
-    </Conteudo>
+      <GlobalStyle />
+      <Conteudo>
+        <Titulo>Character Sheet</Titulo>
+        <Entrada
+          type="text"
+          autoCapitalize="words"
+          name="Name"
+          value={name}
+          onChange={handleNameChange}
+          placeholder="Name"
+        />
+        <Entrada
+          type="text"
+          autoCapitalize="words"
+          name="Class"
+          value={classe}
+          onChange={handleClassChange}
+          placeholder="Class"
+        />
+        <Entrada
+          type="text"
+          autoCapitalize="words"
+          name="Ranking"
+          value={ranking}
+          onChange={handleRankingChange}
+          placeholder="Ranking"
+        />
+        <Texto>
+          <Btn onClick={sobeXP}>+</Btn> {xp} <Btn onClick={desceXP}>-</Btn> (XP)
+        </Texto>
+        <Texto>
+          <Btn onClick={sobeFP}>+</Btn> {fp} <Btn onClick={desceFP}>-</Btn> (FP)
+        </Texto>
+      </Conteudo>
+      <Pezinho>
+        &copy; {new Date().getFullYear()} greedthestampede. Todos os direitos
+        reservados.
+      </Pezinho>
+    </>
   );
 }
 
@@ -152,6 +197,6 @@ export async function getServerSideProps(context) {
       RANKING: cookies.RANKING,
       XP: cookies.XP,
       FP: cookies.FP,
-    }
-  }
+    },
+  };
 }
